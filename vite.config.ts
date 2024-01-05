4a5e23331017f8908251ea2056193112339fa60d
Bayu aditya <bayu.aditya@sci.ui.ac.id>
2024-01-05 16:50:59 +0700

    splitting javascript bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,28 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  preview: {
+    port: 3000,
+  },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          if (id.indexOf('/react/') !== -1) {
+            return '@react'
+          }
+          if (id.indexOf('react-router-dom') !== -1) {
+            return '@react-router'
+          }
+          if (id.indexOf('moment') !== -1) {
+            return '@moment'
+          }
+          if (id.indexOf('/react-select/') !== -1) {
+            return '@react-select'
+          }
+        },
+      },
+    },
+  },
 })
